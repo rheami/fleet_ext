16616b4c4f2c3b39aaaefd056cbe82d91a1ae1ae
Michel Rheault <rheaultmichel@gmail.com>
2018-02-20 15:35:31 -0500

    Initial Commit

---
+++ __openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Fleet Extension",
+    'summary': "This is an extension of Fleet Management module",
+    'description': "This is an extension of Fleet Management module",
+    'author': "Michel Rheault, Osha / Sciantek",
+    'website': "",
+    'category': 'Managing vehicles and contracts',
+    'version': '8.0.1',
+    'depends': ['fleet', 'l10n_ca_toponyms'],
+    'data': [
+        'fleet_data.xml',
+        'views/fleet_view.xml',
+        'views/fleet_partner_view.xml',
+        'views/fleet_board_view.xml',
+    ],
+    'installable': True
+}
