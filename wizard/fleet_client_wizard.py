d0eecc30c800481ccba524ebd6f8018c15d735a6
Michel Rheault <rheaultmichel@gmail.com>
2018-05-28 13:39:05 -0400

    Suite wizard

--- wizard/fleet_client_wizard.py
+++ wizard/fleet_client_wizard.py
@@ -100,3 +100,19 @@ class FleetClientWizard(models.TransientModel): # voir class stock_transfer_deta
             'target': 'new',
         }
 
+    @api.multi
+    def return_action_to_open(self):
+        """ This opens the xml view specified in xml_id for the vehicles or current retailer """
+        ctx = dict(self._context or {})
+        if ctx.get('xml_id'):
+            res = self.env['ir.actions.act_window'].for_xml_id('fleet_ext', ctx.get('xml_id'))
+            res['context'] = ctx
+            if self.vehicle_id.id:
+                res['domain'] = [('id', '=', self.vehicle_id.id)]
+                res['res_id'] = self.vehicle_id.id
+                res['target'] = 'current'
+            else:
+                res['target'] = 'new'
+            res['flags'] = {'form': {'action_buttons': True, 'options': {'mode': 'edit'}}}
+            return res
+        return False
